4b85c3a395f9a806d9eeeb39ae629079ca56852f
furuya <furuya0427@gmail.com>
2018-10-31 22:25:53 +0530

    first commit

--- aibot-d.py
+++ aibot-d.py
@@ -6,10 +6,9 @@ import json
 from datetime import datetime
 
 client = discord.Client()
-token = 'NTAxNzUxNzYwNDI0MDA5NzQw.Dqd8bQ.VL4LkCCqoojc6TJBhBvySPkVakQ'
+token = 'XXXXXXXXXXXXXXXXXXX'
 
-apikey = '6963576a624d31473343366767302e2f7770753564442e4f5832344c566c524942686b6b755553614c5434'
-endpoint = 'https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY=REGISTER_KEY'  
+apikey = 'XXXXXXXXXXXXXXXXXXXX'
 url = endpoint.replace('REGISTER_KEY',apikey)
 
 
@@ -20,7 +19,7 @@ def ai_response(msg,time = ''):
     'appSendTime' : datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
     'voiceText' : msg,
     'botId': 'Chatting',
-    'appId':'59f4939f-0d96-40ef-9180-276e0c3b9711'
+    'appId':'XXXXXXXXXXXXXXXXXXX'
     } 
     headers = {'Content-type': 'application/json;charset=UTF-8'}
     r = requests.post(url, data=json.dumps(payload), headers=headers)
@@ -40,7 +39,7 @@ async def on_message(message):
     if client.user.id in message.content:
        if client.user != message.author:
           msg = message.content
-          msg = msg.lstrip("<@501751760424009740>")       
+          msg = msg.lstrip("<XXXXXXXXXXXXXXXXXXXXXX>")       
           response = ai_response(msg)
           reply = response.replace('。','') + 'めう'
           await client.send_message(message.channel,reply)
